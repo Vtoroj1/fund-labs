a3b98b55446fdfa1f7fb0fc1fc637835ec0a0889
Vtoroj1 <vsuchkov7@gmail.com>
2025-11-06 21:30:57 +0300

    minor spelling mistake

--- lab_pack2/lab1/main.c
+++ lab_pack2/lab1/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 1) {
-        printf("Ошибка: нет аргументов. \n");
+        printf("Ошибка: аргументы задаются внутри кода. \n");
         return 1;
     }
 
